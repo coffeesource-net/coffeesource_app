de9738aa290d759b3ef01c6a034c927d1ac0a862
Andrés Quiroga <andres.quiroga@roadhousestudio.co>
2017-11-09 10:53:12 -0500

    Added templatetag for load last ecoinstant profile post

---
+++ app/templatetags/general_tags.py
@@ -0,0 +1,32 @@
+from steem import Steem
+
+from django import template
+
+register = template.Library()
+
+
+@register.assignment_tag
+def get_last_ecoinstant_entry():
+    s = Steem()
+    blog_entries = s.get_blog(
+        account='ecoinstant',
+        entry_id=0,
+        limit=10,
+    )
+
+    for entry in blog_entries:
+        comment = entry['comment']
+        author = comment.get('author')
+
+        if author == 'ecoinstant':
+            entry_dict = {
+                'title': comment.get('title'),
+                'url': 'https://steemit.com/{0}/@{1}/{2}'.format(
+                    comment.get('category'),
+                    author,
+                    comment.get('permlink'),
+                ),
+            }
+
+            return entry_dict
+    return None
