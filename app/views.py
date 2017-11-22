27ba2309d861207f04794e52de945e5f319fc049
Andrés Quiroga <andres.quiroga@roadhousestudio.co>
2017-11-22 18:42:36 -0500

    Papa pepper (#11)
    * Added papa-pepper selfie contest robot
    
    * Added latest days
    
    * I adjusted the English Translation for the Readme.md document

--- app/views.py
+++ app/views.py
@@ -4,6 +4,18 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['easy_backlink'] = True
+
+        return context
+
 
 class NotFoundView(TemplateView):
     template_name = '404.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['easy_backlink'] = True
+
+        return context
