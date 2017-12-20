d3764673d8ee5a3045e1b45f02e2b194aa72fc60
coffeesource-net <33530509+coffeesource-net@users.noreply.github.com>
2017-12-19 23:54:22 -0500

    Dev (#41)
    * Fixed load more posts on users detail for mobile devices (#22)
    
    * Js accounting fixies (#23)
    
    * Fixed load more posts on users detail for mobile devices
    
    * Fixed button text
    
    * Fixed bad code
    
    * Actualized by adding days 48, 49, 50, 51, 52 and 53 (#30)
    
    * actualize with the final days 54, 55, 56, and 57
    
    * Adding formating for automatic back-link copy pasting
    
    * =Add a static list of season one competitors that are album generatorable
    
    * ==Change to a table with smaller font, added bootstrap table class so that copy and paste creates a table of contents list.
    
    * Added easy backlinking images functionality
    
    * Title filter (#38)
    
    * Added title filter for easy backlink list
    
    * Applying title search on load more posts action
    
    * Images list filter form (#40)
    
    * Added filter form for easy images backlinking functionality

--- app/static/js/accounts/load_posts.js
+++ app/static/js/accounts/load_posts.js
@@ -1,4 +1,4 @@
-function bindScroll(){
+function LoadPosts(){
   var last_loaded_id = $(".CSEntriesList tr:last").data('entry-id');
   var load_account_posts_url = $('.CSEntriesList').data('ax-load-account-posts-url') + '&last_entry_id=' + last_loaded_id;
 
@@ -45,8 +45,7 @@ function bindScroll(){
                 $(obj).hide();
               }
             }
-          });          
-          
+          });
       },
   });
 }
@@ -58,61 +57,9 @@ $(function () {
         if ($(this).hasClass('enabledLoad')) {
           $('.CSAccountLoadPostsSpinner').show();
           $(this).removeClass('enabledLoad');
-          bindScroll();
+          LoadPosts();
         }
 
         return false;
-    });
-
-    // Posts search by title.
-    $('#id_title_search_field').on('input', function() {
-      var search_text = $(this).val().toLowerCase();
-
-      $('.CSEntriesList tr').each(function(i, obj) {
-        var title = $(obj).data('entry-title').toLowerCase();
-
-        if (!title.includes(search_text)) {
-          $(obj).hide();
-        } else {
-          $(obj).show();
-        }
-      });
-    });
-
-    // Filter list by tag - Form submit.
-    $('.backlink_filter_form form').on('submit', function() {
-      var tag = $('#id_tag_filter_field').val();
-      var data_tag = 'tag_' + tag;
-      var tag_element = '<li data-tag="' + data_tag + '"><span class="badge badge-primary">' + tag + '</span> <a href="javascript:void(0)" class="fa fa-times tag_remove" aria-hidden="true"></a></li>';
-      $('.applied_tag_filters').append(tag_element);
-
-      $('.CSEntriesList tr').not("." + data_tag).hide();
-      $('#id_tag_filter_field').val('');
-
-      return false;
-    });
-
-    // Remove tag filter.
-    $('.applied_tag_filters').on('click', '.tag_remove', function () {
-      var tag_to_remove = $(this).closest('li').data('tag');
-
-      var search_text = $('#id_title_search_field').val().toLowerCase();
-
-      $('.CSEntriesList tr').each(function(i, obj) {
-
-        // Check if there is any title filter appliced.
-        var title = $(obj).data('entry-title').toLowerCase();
-        if (!$(obj).hasClass(tag_to_remove)) {
-          $(obj).show();
-
-          if (search_text) {
-            if (!title.includes(search_text)) {
-              $(obj).hide();              
-            }
-          }
-        }
-      });
-
-      $(this).parent().remove();
     });
 });
