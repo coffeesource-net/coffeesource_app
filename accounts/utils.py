60e5984c7f70ef855d2f70c7c70158c32209c484
not-a-bird <33614331+not-a-bird@users.noreply.github.com>
2018-01-13 11:32:29 -0500

    Utils did extra work (#54)
    * modified the backlinks to display with relative names but still allow
    clicking to view off steemit.com
    
    * Reduced work done by the `get_user_posts` function.
    
    The `get_user_posts` function was creating the dictionary for the entry even if it wasn't by the right author, then it was just not adding the dictionary to the list when the author didn't match.  I changed the order here so that it would check the author first, and then only do the work if it was the author it cared about.

--- accounts/utils.py
+++ accounts/utils.py
@@ -21,29 +21,29 @@ def get_user_posts(username, from_id):
         # Could be util to load posts on utopian directly.
         # parent_permlink = comment.get('permlink')
         author = comment.get('author')
-        category = comment.get('category')
-
-        entry_dict = {
-            'id': comment.get('id'),
-            'title': comment.get('title'),
-            'clickable': 'https://www.steemit.com/{0}/@{1}/{2}'.format(
-                category,
-                author,
-                comment.get('permlink'),
-            ),
-            'url': '/{0}/@{1}/{2}'.format(
-                category,
-                author,
-                comment.get('permlink'),
-            ),
-            'author': author,
-            'category': category,
-            'tags': metadata.get('tags'),
-            'images': metadata.get('image'),
-            'entry_id': entry['entry_id']
-        }
 
         if username == author:
+            category = comment.get('category')
+            entry_dict = {
+                'id': comment.get('id'),
+                'title': comment.get('title'),
+                'clickable': 'https://www.steemit.com/{0}/@{1}/{2}'.format(
+                    category,
+                    author,
+                    comment.get('permlink'),
+                ),
+                'url': '/{0}/@{1}/{2}'.format(
+                    category,
+                    author,
+                    comment.get('permlink'),
+                ),
+                'author': author,
+                'category': category,
+                'tags': metadata.get('tags'),
+                'images': metadata.get('image'),
+                'entry_id': entry['entry_id']
+            }
+
             entries_list.append(entry_dict)
 
     return entries_list
